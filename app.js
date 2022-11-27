186da15a408d1612acd23f2e9999c6e5dc1c7dd6
Xotl <7025727+Xotl@users.noreply.github.com>
2022-11-27 12:00:29 -0600

    Adding unit tests (#14)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict'
+
 const { getWaifu } = require('./utils/puppeteerScrapper')
 const weebhook = require('./utils/webhook')
+const url = process.env.WEB_HOOK_URL
 
 async function main() {
   const waifuObj = await getWaifu()
-  await weebhook.sendDiscordMessage(waifuObj)
+  await weebhook.sendDiscordMessage(waifuObj, url)
 }
 
 main()
